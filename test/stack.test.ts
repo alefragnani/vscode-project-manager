23520cf46945bbfba72900ae3ecbd231fba9e726
Alessandro Fragnani <alefragnani@hotmail.com>
2017-05-25 00:22:24 -0300

    moved to src folder

--- test/stack.test.ts
+++ test/stack.test.ts
@@ -9,7 +9,7 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import * as myStack from '../stack';
+import * as myStack from '../src/stack';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Stack Tests", () => {
