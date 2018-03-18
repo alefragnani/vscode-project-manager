f2a712c0dfee34d4fca3bb9156149aa8a09b64b7
Alessandro Fragnani <alefragnani@hotmail.com>
2018-03-17 22:20:55 -0300

    clean up in gitLocator

--- src/gitLocator.ts
+++ src/gitLocator.ts
@@ -1,6 +1,6 @@
 import fs = require("fs");
 import path = require("path");
-import { CustomProjectLocator, CustomRepositoryDetector } from "./abstractLocator";
+import { CustomRepositoryDetector } from "./abstractLocator";
 
 export class GitRepositoryDetector extends CustomRepositoryDetector {
 
