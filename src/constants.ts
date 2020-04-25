2ab46f9041cc4e66c97001619f266021a9ae0b6a
Alessandro Fragnani <alefragnani@hotmail.com>
2020-04-25 14:32:45 -0300

    Added OpenInCurrentWindowIfEmpty option (Closes #188)

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const PROJECTS_FILE = "projects.json";
+
+export enum CommandLocation { CommandPalette, SideBar, StatusBar };
+
+export enum OpenInCurrentWindowIfEmptyMode {
+    always = "always",
+    onlyUsingCommandPalette = "onlyUsingCommandPalette",
+    onlyUsingSideBar = "onlyUsingSideBar",
+    never = "never"
+}
