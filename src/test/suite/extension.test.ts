54d67595667ab37204464ccb938e6de2f7c78f00
Alessandro Fragnani <alefragnani@hotmail.com>
2020-09-20 20:05:20 -0300

    Add basic tests

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,31 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+
+const timeout = async (ms = 200) => new Promise(resolve => setTimeout(resolve, ms));
+
+suite('Extension Test Suite', () => {
+	let extension: vscode.Extension<any>;
+	vscode.window.showInformationMessage('Start all tests.');
+
+	suiteSetup(() => {
+		extension = vscode.extensions.getExtension('alefragnani.project-manager') as vscode.Extension<any>;
+	});
+
+	test('Sample test', () => {
+		assert.equal(-1, [1, 2, 3].indexOf(5));
+		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+
+	test('Activation test', async () => {
+		await extension.activate();
+		assert.equal(extension.isActive, true);
+	});
+
+	test('Extension loads in VSCode and is active', async () => {
+		await timeout(1500);
+		assert.equal(extension.isActive, true);
+    });
+});
