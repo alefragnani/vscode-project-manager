12659674457a480786221b8a35be8941768dc3b3
Alessandro Fragnani <alefragnani@hotmail.com>
2021-07-30 21:19:51 -0300

    Add settings change detection to update sortBy Context

--- src/extension.ts
+++ src/extension.ts
@@ -165,6 +165,7 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         if (cfg.affectsConfiguration("projectManager.sortList")) {
+            updateSortByContext();
             providerManager.storageProvider.refresh();
             providerManager.updateTreeViewStorage();
         }
