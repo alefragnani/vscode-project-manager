6192262fbf938e0b8c14433c1bee6719ab78c08f
Alessandro Fragnani <alefragnani@hotmail.com>
2022-08-10 19:53:48 -0300

    Add proper "Review in File Manager" command for Linux (Closes #618)

--- src/commands/revealFileInOS.ts
+++ src/commands/revealFileInOS.ts
@@ -5,20 +5,20 @@
 
 import { commands, Uri, window } from "vscode";
 import { Container } from "../../vscode-project-manager-core/src/container";
-import { isRemotePath, isWindows } from "../../vscode-project-manager-core/src/utils/remote";
+import { isMacOS, isRemotePath, isWindows } from "../../vscode-project-manager-core/src/utils/remote";
 
 async function revealFileInOS(node: any) {
     if (!node) { return }
 
     if (isRemotePath(node.command.arguments[ 0 ])) {
-        const revealApp = isWindows ? "Explorer" : "Finder";
+        const revealApp = isWindows ? "Explorer" : isMacOS ? "Finder" : "File Manager";
         window.showErrorMessage(`Remote projects can't be revealed in ${revealApp}`);
     }
 
     commands.executeCommand("revealFileInOS", Uri.file(node.command.arguments[ 0 ]))
 }
 export function registerRevealFileInOS() {
-    commands.executeCommand("setContext", "isWindows", isWindows);
     Container.context.subscriptions.push(commands.registerCommand("_projectManager.revealInFinder#sideBar", (node) => revealFileInOS(node)));
     Container.context.subscriptions.push(commands.registerCommand("_projectManager.revealInExplorer#sideBar", (node) => revealFileInOS(node)));
+    Container.context.subscriptions.push(commands.registerCommand("_projectManager.revealInFileManager#sideBar", (node) => revealFileInOS(node)));
 }
